1eaf9ea25d0cfbc1117465a6e1ca6f1b14cb5380
samuelmaxi <samuelgaraujo87@gmail.com>
2023-09-22 11:55:56 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/samuelmaxi/first-module-golang
+
+go 1.21.1
---
+++ math.go
@@ -0,0 +1,14 @@
+package firstmodulegolang
+
+func Sum(x int, y int) int {
+	return x + y
+}
+func Div(x int, y int) int {
+	return x / y
+}
+func Mul(x int, y int) int {
+	return x * y
+}
+func Sub(x int, y int) int {
+	return x - y
+}
